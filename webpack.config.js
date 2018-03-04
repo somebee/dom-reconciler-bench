5115f6b859ca01bfdb0027ec82f4e6519a77857c
Sindre Aarsaether <sindre@identu.no>
2018-03-04 13:27:43 +0100

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,33 +4,28 @@ const imba = {
 	entry: "./apps/imba/app.imba",
 	output: { filename: "./apps/imba/app.js" },
 	resolve: { extensions: [".imba",".js", ".json"] },
-	module: { rules: [{ test: /.imba$/, loader: 'imba/loader' }] }
+	module: { rules: [{ test: /\.imba$/, loader: 'imba/loader' }] }
 }
 
-/*
 const react = {
-  entry: './apps/react/test.jsx',
+  entry: './apps/react/app.jsx',
   output: {
-    filename: './apps/react/testout.js'
+    filename: './apps/react/app.js'
   },
+  resolve: { extensions: [".jsx",".js", ".json"] },
   module: {
     rules: [
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
       { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/, options: {presets: ['es2015', 'react']} }
     ]
   }
-} */
-
-const react = {
-	entry: "./apps/react/js/app.js",
-	output: { filename: "./apps/react/app.js" }
 }
 
 const client = {
 	entry: "./client.imba",
 	output: { filename: "./client.js" },
 	resolve: { extensions: [".imba",".js", ".json"] },
-	module: { rules: [{ test: /.imba$/, loader: 'imba/loader' }] }
+	module: { rules: [{ test: /\.imba$/, loader: 'imba/loader' }] }
 }
 
 module.exports = [imba,react,client];
